dafd311afe2ce42f3e1574d11c0155298c9e4db7
Sparkfire <48664233+Sparkfir3@users.noreply.github.com>
2024-02-17 15:37:38 -0600

    Import quick outline; Robot is highlighted properly when mouse hovers over it

---
+++ Assets/_Main/Scripts/ObjectOutline/ObjectOutlineController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectOutlineController : MonoBehaviour
+{
+    [SerializeField]
+    private float raycastDistance = 1000f;
+    [SerializeField]
+    private LayerMask raycastLayers = ~0;
+
+    // -----------------------------------------------------
+
+    [SerializeField]
+    private OutlineableObject _targetOutline;
+    public OutlineableObject TargetOutline {
+        get {
+            return _targetOutline;
+        }
+        private set {
+            if(value != _targetOutline) {
+                if(_targetOutline) {
+                    _targetOutline.SetOutlineActive(false);
+                }
+                _targetOutline = value;
+                if(_targetOutline) {
+                    _targetOutline.SetOutlineActive(true);
+                }
+            }
+        }
+    }
+
+    private Ray RayFromCamera => Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    // -----------------------------------------------------
+
+    private void LateUpdate() {
+        if(Physics.Raycast(RayFromCamera, out RaycastHit hit, raycastDistance, raycastLayers)) {
+            TargetOutline = hit.collider.GetComponentInParent<OutlineableObject>();
+        } else {
+            TargetOutline = null;
+        }
+    }
+
+}
---
+++ Assets/_Main/Scripts/ObjectOutline/OutlineableObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineableObject : MonoBehaviour
+{
+    [SerializeField]
+    private Outline outline;
+
+    // -----------------------------------------------------
+
+    public void SetOutlineActive(bool active) {
+        if(!outline) {
+            return;
+        }
+        outline.enabled = active;
+    }
+
+}
